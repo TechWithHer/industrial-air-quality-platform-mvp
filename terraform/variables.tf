3d05a810c7ca260c4aead3409803a861d08c909a
Ayushi Singh <140041893+TechWithHer@users.noreply.github.com>
2026-07-18 12:31:52 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
 
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "iaqap"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
