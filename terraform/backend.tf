2ecd4d163484d5a9636f756035481bba187851f7
Ayushi Singh <140041893+TechWithHer@users.noreply.github.com>
2026-07-18 12:34:29 +0800

    Configure Terraform backend with S3 settings

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "iaqap-dev-tf-state"
+    key            = "iaqap/iaqap-dev-tf-state"
+    region         = "ap-southeast-1"
+    use_lockfile   = true
+  }
+}
