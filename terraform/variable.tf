693cce83f5bdd8a35f1f24b1d046f3a113916453
Ayushi Singh <140041893+TechWithHer@users.noreply.github.com>
2026-07-19 08:45:21 +0800

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -12,3 +12,10 @@ variable "athena_results_bucket_name" {
   type    = string
   default = "iaqap-dev-athena-results"
 }
+variable "aws_region" {
+  description = "AWS Region"
+
+  type = string
+
+  default = "ap-southeast-1"
+}
