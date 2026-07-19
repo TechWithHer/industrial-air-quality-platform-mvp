4bac827c6be863407e95301979ba7b4e3d679999
Ayushi Singh <140041893+TechWithHer@users.noreply.github.com>
2026-07-19 17:57:45 +0800

    Add EventBridge target and Lambda permission

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -7,3 +7,24 @@ resource "aws_cloudwatch_event_rule" "collector_schedule" {
   schedule_expression = "rate(15 minutes)"
 
 }
+
+resource "aws_cloudwatch_event_target" "collector_target" {
+
+  rule = aws_cloudwatch_event_rule.collector_schedule.name
+
+  arn = aws_lambda_function.collector.arn
+
+}
+resource "aws_lambda_permission" "allow_eventbridge" {
+
+  statement_id = "AllowExecutionFromEventBridge"
+
+  action = "lambda:InvokeFunction"
+
+  function_name = aws_lambda_function.collector.function_name
+
+  principal = "events.amazonaws.com"
+
+  source_arn = aws_cloudwatch_event_rule.collector_schedule.arn
+
+}
