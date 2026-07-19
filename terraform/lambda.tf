3755d7082980cc4fe101ef955a43615ae8b1656a
Ayushi Singh <140041893+TechWithHer@users.noreply.github.com>
2026-07-19 13:44:00 +0800

    Define CloudWatch log group and Lambda function
    Added AWS CloudWatch log group and Lambda function resources.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,3 +3,40 @@ data "archive_file" "collector_zip" {
   source_dir  = "../lambdas/collector"
   output_path = "../lambdas/collector.zip"
 }
+
+resource "aws_cloudwatch_log_group" "collector" {
+  name              = "/aws/lambda/iaqap-dev-collector"
+  retention_in_days = 14
+}
+
+resource "aws_lambda_function" "collector" {
+
+  function_name = "iaqap-dev-collector"
+
+  role = aws_iam_role.collector_lambda_role.arn
+
+  handler = "app.lambda_handler"
+
+  runtime = "python3.11"
+
+  filename         = data.archive_file.collector_zip.output_path
+  source_code_hash = data.archive_file.collector_zip.output_base64sha256
+
+  timeout = 30
+
+  memory_size = 256
+
+  environment {
+
+    variables = {
+
+      RAW_BUCKET = aws_s3_bucket.raw.bucket
+
+    }
+
+  }
+
+  depends_on = [
+    aws_cloudwatch_log_group.collector
+  ]
+}
