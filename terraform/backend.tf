5234e099122c983229028fc4481868eb714ef0c8
Ayushi Singh <140041893+TechWithHer@users.noreply.github.com>
2026-07-19 08:49:51 +0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "iaqap-dev-tf-state"
-    key            = "iaqap/iaqap-dev-tf-state"
-    region         = "ap-southeast-1"
-    use_lockfile   = true
+    bucket       = "iaqap-dev-tf-state"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-southeast-1"
+    use_lockfile = true
   }
 }
