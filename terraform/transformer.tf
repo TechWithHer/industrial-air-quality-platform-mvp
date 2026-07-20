96141eb0b2ca257b83364aa866fb7fcae0327d29
Ayushi <ayushi2530@gmail.com>
2026-07-20 15:51:01 +0800

    fmt

--- terraform/transformer.tf
+++ terraform/transformer.tf
@@ -49,7 +49,7 @@ data "aws_iam_policy_document" "transformer_policy" {
 
 resource "aws_iam_policy" "transformer_policy" {
 
-  name   = "iaqap-dev-transformer-policy"
+  name = "iaqap-dev-transformer-policy"
 
   policy = data.aws_iam_policy_document.transformer_policy.json
 }
@@ -67,7 +67,7 @@ resource "aws_iam_role" "transformer_lambda_role" {
 
 resource "aws_iam_role_policy_attachment" "transformer_policy_attachment" {
 
-  role       = aws_iam_role.transformer_lambda_role.name
+  role = aws_iam_role.transformer_lambda_role.name
 
   policy_arn = aws_iam_policy.transformer_policy.arn
 }
@@ -107,7 +107,7 @@ resource "aws_lambda_function" "transformer" {
 
 resource "aws_cloudwatch_log_group" "transformer" {
 
-  name              = "/aws/lambda/iaqap-dev-transformer"
+  name = "/aws/lambda/iaqap-dev-transformer"
 
   retention_in_days = 14
 }
